dcebcb1238badbbb52c81b90d6d6ca28a1e28c45
ation_ciger <ation_ciger@sgxi.cn>
2023-01-11 14:50:14 +0800

    😪

--- device.mk
+++ device.mk
@@ -22,32 +22,196 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
 PRODUCT_TARGET_VNDK_VERSION := 33
 
-LOCAL_PATH := device/xiaomi/matisse
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
 
-# A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
+    FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=erofs \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script
+
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio.common-util \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio@4.0-util \
+    android.hardware.audio@5.0-util \
+    android.hardware.audio@6.0-util \
+    android.hardware.audio@7.0-util \
+    android.hardware.audio.effect@4.0-util \
+    android.hardware.audio.effect@5.0-util \
+    android.hardware.audio.effect@6.0-util \
+    android.hardware.audio.effect@7.0-util \
+    libaudio-resampler \
+    libaudioprocessing \
+    libaudiospdif
+
+PRODUCT_PACKAGES += \
+    android.hardware.soundtrigger@2.0 \
+    android.hardware.soundtrigger@2.1 \
+    android.hardware.soundtrigger@2.2
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2.recovery
+
+# Config store
+PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.0-service \
+    android.hardware.configstore@1.1-service 
+
+# Display
+PRODUCT_PACKAGES += \
+    libvulkan
+
+# DT2W
+PRODUCT_PACKAGES += \
+    DT2W-Service-MT6895
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.allocator@1.0 \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    android.hidl.memory.block@1.0 \
+    libhidltransport \
+    libhwbinder 
+
+# IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
 
 PRODUCT_PACKAGES += \
-    bootctrl.mt6983
+    libshim_vtservice
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6983 \
-    libgptutils \
-    libz \
-    libcutils
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
+
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-fpc.idc \
+    $(LOCAL_PATH)/configs/idc/uinput-goodix.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-goodix.idc \
+    $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
+
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.xiaomi_mt6983
+
+# MIUI Camera
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/default-permissions/miuicamera-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/miuicamera-permissions.xml \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuicamera.xml
+
+# MtkInCallService
+PRODUCT_PACKAGES += \
+    MtkInCallService
+
+# MTK plpath utils
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/mtk_plpath_utils:$(TARGET_COPY_OUT_SYSTEM)/bin/mtk_plpath_utils \
+    $(LOCAL_PATH)/prebuilts/mtk-plpath-utils.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/mtk-plpath-utils.rc \
+    $(LOCAL_PATH)/prebuilts/mtk_plpath_utils:recovery/root/system/bin/mtk_plpath_utils \
+    $(LOCAL_PATH)/prebuilts/mtk-plpath-utils.rc:recovery/root/system/etc/init/mtk-plpath-utils.rc
+
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.2-service \
+    com.android.nfc_extras \
+    libchrome.vendor \
+    NfcNci \
+    SecureElement \
+    Tag
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
+    RubensFrameworkResOverlay \
+    TetheringOVerlay \
+    WifiOverlay \
+    SystemUIOverlay \
+    SettingsOverlay
+
+PRODUCT_PACKAGES += \
+    FrameworkResOverlay \
+    FrameworkResOverlayExt
+
+# Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Power
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/power/powerscntbl.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/powerscntbl.xml
+
+# Recovery
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/root/init.recovery.mt6983.rc:recovery/root/init.recovery.mt6983.rc \
+    $(LOCAL_PATH)/prebuilts/modules/ramdisk/modules.load.recovery:recovery/root/lib/modules/modules.load.recovery
+
+# RIL
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.6 \
+    android.hardware.radio.config@1.3 \
+    android.hardware.secure_element@1.2
+
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.mt6983.rc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.mt6983:recovery/root/first_stage_ramdisk/fstab.mt6983
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 33
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Sysconfig
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sysconfig/miuicamera-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuicamera-hiddenapi-package-whitelist.xml
+
+# VNDK
+PRODUCT_EXTRA_VNDK_VERSIONS := 33
+
+# Xiaomi Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/xiaomi/matisse/matisse-vendor.mk)
