b609b2b342e49d281e7ddef01ff703a196ac2701
ation_ciger <ation_ciger@sgxi.cn>
2023-01-21 11:21:27 +0800

    fix ramdisk modules path

--- device.mk
+++ device.mk
@@ -35,7 +35,7 @@ PRODUCT_PACKAGES += \
     SettingsOverlay
 # Recovery
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilts/modules/modules.load.recovery:recovery/root/lib/modules/modules.load.recovery
+    $(LOCAL_PATH)/prebuilts/modules/ramdisk/modules.load.recovery:recovery/root/lib/modules/modules.load.recovery
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilts/firmware/,recovery/root/vendor/firmware)
