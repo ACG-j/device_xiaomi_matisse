2907db1e087507b4ac8962c26fd05035286f66fa
ation_ciger <ation_ciger@sgxi.cn>
2023-01-29 11:51:50 +0800

    fix set_permissive.rc path

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -45,11 +45,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.mt6983.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := set_permissive.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init/hw/set_permissive.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
